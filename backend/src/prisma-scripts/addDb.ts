c68d9adb93429063cb0ff9f77aacff40072bd2d5
Faisal <faisal@owimer.co>
2024-10-26 14:11:59 -0400

    backedn changes

--- backend/src/prisma-scripts/addDb.ts
+++ backend/src/prisma-scripts/addDb.ts
@@ -3,7 +3,10 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient({})
 
 const main = async () => {
+  console.log('Starting data creation process...')
+
   // Create users
+  console.log('Creating user1...')
   const user1 = await prisma.user.create({
     data: {
       email: 'user1@example.com',
@@ -11,9 +14,12 @@ const main = async () => {
       firstName: 'Michael',
       lastName: 'Scott',
       avatar: 'https://github.com/shadcn.png',
+      clerkId: 'user_1',
     },
   })
+  console.log('User1 created:', user1)
 
+  console.log('Creating user2...')
   const user2 = await prisma.user.create({
     data: {
       email: 'user2@example.com',
@@ -21,10 +27,26 @@ const main = async () => {
       firstName: 'Dwight',
       lastName: 'Schrute',
       avatar: '',
+      clerkId: 'user_2',
     },
   })
+  console.log('User2 created:', user2)
+
+  console.log('Creating user3...')
+  const user3 = await prisma.user.create({
+    data: {
+      email: 'faisal@example.com',
+      username: 'Faisalowimer',
+      firstName: 'Faisal',
+      lastName: 'Owimer',
+      avatar: 'https://github.com/faisalowimer.png',
+      clerkId: 'user_2nxCrgCsPklrlkQ6Uu0NxvHXvE1',
+    },
+  })
+  console.log('User3 created:', user3)
 
   // Create artworks
+  console.log('Creating artwork1...')
   const artwork1 = await prisma.artWork.create({
     data: {
       configuration: {
@@ -36,42 +58,65 @@ const main = async () => {
       author: { connect: { id: user1.id } },
     },
   })
+  console.log('Artwork1 created:', artwork1)
 
+  console.log('Creating artwork2...')
   const artwork2 = await prisma.artWork.create({
     data: {
       configuration: {
         colorA: { h: 120, s: 70, b: 65 },
         colorB: { h: 300, s: 80, b: 70 },
-        stripeCount: 15,
+        stripeCount: 8,
         style: 'circle',
       },
       author: { connect: { id: user2.id } },
     },
   })
+  console.log('Artwork2 created:', artwork2)
+
+  console.log('Creating artwork3 for user3...')
+  const artwork3 = await prisma.artWork.create({
+    data: {
+      configuration: {
+        colorA: { h: 60, s: 90, b: 85 },
+        colorB: { h: 330, s: 75, b: 75 },
+        stripeCount: 12,
+        style: 'line',
+      },
+      author: { connect: { id: user3.id } },
+    },
+  })
+  console.log('Artwork3 created:', artwork3)
 
   // Create likes
-  await prisma.like.create({
+  console.log('Creating like for user1 on artwork2...')
+  const like1 = await prisma.like.create({
     data: {
       user: { connect: { id: user1.id } },
       artWorks: { connect: { id: artwork2.id } },
     },
   })
+  console.log('Like1 created:', like1)
 
-  await prisma.like.create({
+  console.log('Creating like for user2 on artwork1...')
+  const like2 = await prisma.like.create({
     data: {
       user: { connect: { id: user2.id } },
       artWorks: { connect: { id: artwork1.id } },
     },
   })
+  console.log('Like2 created:', like2)
 
   console.log('Data created successfully.')
 }
 
 main()
   .catch((err) => {
-    console.error(err)
+    console.error('Error occurred during data creation:', err)
     process.exit(1)
   })
   .finally(async () => {
+    console.log('Disconnecting from Prisma...')
     await prisma.$disconnect()
+    console.log('Disconnected from Prisma.')
   })
